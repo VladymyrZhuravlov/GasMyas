dc943e16f3cdcb681e0ec09dd080b85afbd70e04
vzhuravlev <vzhuravlev@itteam.io>
2017-10-24 14:40:50 +0300

    lol

--- app/src/main/java/io/itteam/gasmyas/RetroClient.java
+++ app/src/main/java/io/itteam/gasmyas/RetroClient.java
@@ -1,22 +1,19 @@
 package io.itteam.gasmyas;
 
 
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 public class RetroClient {
 
-//    private static final String ROOT_URL = "https://maps.googleapis.com";
-//
-//    private static Retrofit getRetrofitInstance() {
-//        return new Retrofit.Builder()
-//                .baseUrl(ROOT_URL)
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .build();
-//    }
-//
-//    public static GetRoute getRoute() {
-//        return getRetrofitInstance().create(GetRoute.class);
-//    }
+    private static final String ROOT_URL = "http://192.168.9.220:44444";
+
+    private static Retrofit getRetrofitInstance() {
+        return new Retrofit.Builder()
+                .baseUrl(ROOT_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static GetRoute getRoute() {
+        return getRetrofitInstance().create(GetRoute.class);
+    }
 
 }
