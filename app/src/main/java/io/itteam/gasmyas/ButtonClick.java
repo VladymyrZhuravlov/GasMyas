98467489656b1b6b2cd13e5b557add02ec535ca6
Rokitskiy Serhiy <srokitskiy@itteam.io>
2017-10-24 18:14:09 +0300

    push

--- app/src/main/java/io/itteam/gasmyas/ButtonClick.java
+++ app/src/main/java/io/itteam/gasmyas/ButtonClick.java
@@ -2,20 +2,51 @@ package io.itteam.gasmyas;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
-public class ButtonClick extends AppCompatActivity {
+import com.medialablk.easytoast.EasyToast;
 
-    EditText number;
+public class ButtonClick extends AppCompatActivity implements View.OnClickListener {
+
+    private Button connectBtn;
+    private boolean isConnect = false;
+    private ImageView statusImage;
+    private EditText number;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
 
         number = (EditText)findViewById(R.id.numberSch);
+        connectBtn = (Button) findViewById(R.id.connectBtn);
+        statusImage = (ImageView) findViewById(R.id.statusImage);
+        connectBtn.setOnClickListener(this);
+
     }
 
     public void btnClick(){
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (!number.getText().toString().equals("")) {
+            if (isConnect) {
+                statusImage.setImageResource(R.drawable.round_error);
+                isConnect = false;
+                connectBtn.setText("Активировать");
+            } else {
+                statusImage.setImageResource(R.drawable.round_success);
+                isConnect = true;
+                connectBtn.setText("Деактивировать");
+            }
+        } else {
+            EasyToast.error(ButtonClick.this, "Введите номер!!!");
+        }
 
     }
 }
