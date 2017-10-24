b93f40d31bbb199c5c1ed81f2a13bb462fe40b33
vzhuravlev <vzhuravlev@itteam.io>
2017-10-24 12:29:06 +0300

    lol

---
+++ app/src/main/java/io/itteam/gasmyas/Code.java
@@ -0,0 +1,13 @@
+package io.itteam.gasmyas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Code extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code);
+    }
+}
