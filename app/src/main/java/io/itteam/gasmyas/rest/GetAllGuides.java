3d50d09d0f7bafdb099d67fd1e1c1539a8031af3
vzhuravlev <vzhuravlev@itteam.io>
2017-10-24 12:37:50 +0300

    lol

---
+++ app/src/main/java/io/itteam/gasmyas/rest/GetAllGuides.java
@@ -0,0 +1,15 @@
+package io.itteam.gasmyas.rest;
+
+import io.itteam.travel.json.guides.Guides;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+
+public interface GetAllGuides {
+
+    @Headers({"Content-Type: application/json",
+            "X-TenantId: ittclientid_00001"})
+    @GET("/blog/v1/guide?preview")
+    Call<Guides> getTask();
+
+}
---
+++ app/src/main/java/io/itteam/gasmyas/rest/PostAuthorization.java
@@ -0,0 +1,19 @@
+package io.itteam.gasmyas.rest;
+
+
+import io.itteam.travel.json.authorization.AuthorizationData;
+import io.itteam.travel.json.authorization.Response;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface PostAuthorization {
+
+    @Headers({"Content-Type: application/json",
+            "X-TenantId: ittclientid_00001",
+            "Authorization: Basic YWU3NWVhMDMtYTRkYS00NTA2LThlZWYtNmY0ZmQ5YTBmZWYyLWdRZEJpbjRIN0wzOUVGYnY6djdvQTRFSmU4U01kbEEzREZtS0lQUkhwczl5ZVlNR2Q="})
+    @POST("/account/v1/user/signin")
+    Call<Response> registerUser(@Body AuthorizationData registrationBody);
+
+}
