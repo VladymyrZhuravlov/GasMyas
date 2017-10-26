3b36508e0c158c37a20546184d86b4de129a2bc3
Rokitskiy Serhiy <srokitskiy@itteam.io>
2017-10-26 15:03:46 +0300

    push

---
+++ app/src/main/java/io/itteam/gasmyas/rest/GetDeactivated.java
@@ -0,0 +1,17 @@
+package io.itteam.gasmyas.rest;
+
+
+import io.itteam.gasmyas.json.activated.Activator;
+import io.itteam.gasmyas.json.user.User;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
+
+public interface GetDeactivated {
+
+    @Headers({"Content-Type: application/json"})
+    @GET("/api/v1/count/deactivated")
+    Call<Activator> getTask(@Header("Authorization") String v, @Query("count") String phone);
+}
