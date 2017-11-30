a0d54cee611a2fd932dc0750dd307f888f4df481
vzhuravlev <vzhuravlev@itteam.io>
2017-11-30 17:27:34 +0200

    add Resalt

--- app/src/main/java/io/itteam/gasmyas/ButtonClick.java
+++ app/src/main/java/io/itteam/gasmyas/ButtonClick.java
@@ -6,17 +6,13 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.medialablk.easytoast.EasyToast;
 
